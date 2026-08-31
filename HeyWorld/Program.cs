247cdf386a9948c0cb960d3c775aa707237036e9
WatermelonQuokka <danielchis123@hotmail.com>
2026-08-31 11:59:57 +0200

    adding readlines for user inputs

--- HeyWorld/Program.cs
+++ HeyWorld/Program.cs
@@ -15,14 +15,45 @@ Console.WriteLine("This is a simple C# program.");
 // int is a data type that represents whole numbers
 // double is a data type that represents decimal numbers
 // bool is a data type that represents true or false values
-string name = "Bjorn";
-int age = 36;
-double height = 1.75;
+
+Console.Write("Vad heter du? ");
+string? name = Console.ReadLine();
+Console.Write("Hur gammal är du? ");
+string? ageAsString = Console.ReadLine();
+
+
+// ! = not reverses true/false in a condition
+// int.TryParse to parse a string to a number
+// if it succeeds the number will be in the variable
+// that we specified after out int
+
+if (!int.TryParse(ageAsString, out int age))
+{
+    // if we failed
+    age = 5;
+    Console.WriteLine("Eftersom vi inte uppfattade din ålder räknar vi med att du är 5 år");
+}
+
+Console.WriteLine("Hur lång är du i meter? ");
+string? heightAsString = Console.ReadLine();
+
+
+if (!double.TryParse(heightAsString, out double height))
+{
+    // if we failed
+    height = 0.5;
+    Console.WriteLine("Eftersom vi inte uppfattade din längd räknar vi med att du är 0,5 m");
+}
+
 bool isDeveloper = true;
+
+
 // Alternative to string interpolation
 //string concatenation with + :
 // Console.WriteLine("Jag heter " + name + "!");
+
 Console.WriteLine($"Jag heter {name} och är {age} år gammal!");
+Console.WriteLine($"Om 10 år så är jag {age + 10} år gammal!");
 Console.WriteLine($"Jag är {height} meter lång.");
 
 // if and a condition within parenthesis that is true
@@ -41,3 +72,4 @@ else
     Console.WriteLine("Jag är inte utvecklare.");
     Console.WriteLine("Jag kan inget om programmering.");
 } 
+
