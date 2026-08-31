7b76e571bd5f924dad5c91f0634a342641e3ce5e
WatermelonQuokka <danielchis123@hotmail.com>
2026-08-31 10:16:09 +0200

    learning console.writeline

---
+++ HeyWorld/Program.cs
@@ -0,0 +1,9 @@
+﻿// Console is an object
+// that represents the terminal
+// and has a method named WriteLine
+// that we can tell
+// with a string as argument
+// then string will then be written
+// to the terminal
+Console.WriteLine("Hello, World!");
+Console.WriteLine("This is a simple C# program.");
